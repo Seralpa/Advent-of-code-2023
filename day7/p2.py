b6e03a85fae157ff67bf246c21b48a2f9a48ecbb
seralpa <seralpa789@gmail.com>
2023-12-07 19:41:18 +0100

    refactor day 7

--- day7/p2.py
+++ day7/p2.py
@@ -1,10 +1,21 @@
 from dataclasses import dataclass
+from enum import Enum
 from functools import total_ordering
 from collections import Counter
 import os
 from typing import Self
 
 card2value = {"A": 13, "K": 12, "Q": 11, "J": 0, "T": 9, "9": 8, "8": 7, "7": 6, "6": 5, "5": 4, "4": 3, "3": 2, "2": 1}
+
+
+class HandType(Enum):
+	HIGH_CARD = 0
+	PAIR = 1
+	TWO_PAIR = 2
+	TRIO = 3
+	FULL_HOUSE = 4
+	POKER = 5
+	REPOKER = 6
 
 
 @dataclass
@@ -14,43 +25,43 @@ class Hand:
 	bid: int
 
 	@property
-	def type(self) -> int:
-		count = Counter(self.cards).most_common()
-		if count[0][1] == 4:
-			if "J" in self.cards:
-				return 6
-			return 5  # poker
-		if count[0][1] == 5:
-			return 6  # repoker
-		if count[0][1] == 1:
-			if "J" in self.cards:
-				return 1
-			return 0  # high card
-		if count[0][1] == 2 and count[1][1] == 1:
-			if "J" in self.cards:
-				return 3
-			return 1  # pair
-		if count[0][1] == 2:
-			if self.cards.count("J") == 2:
-				return 5
-			elif "J" in self.cards:
-				return 4
-			return 2  # 2 pair
-		if count[0][1] == 3 and count[1][1] == 1:
-			if "J" in self.cards:
-				return 5
-			return 3  # trio
-		# if count[0][1] == 3:
-		if "J" in self.cards:
-			return 6
-		return 4  # full house
+	def type(self) -> HandType:
+		match Counter(self.cards).most_common():
+			case [(_, 5), *_]:
+				return HandType.REPOKER
+			case [(_, 4), *_] if "J" in self.cards:
+				return HandType.REPOKER
+			case [(_, 4), *_]:
+				return HandType.POKER
+			case [(_, 3), (_, 2), *_] if "J" in self.cards:
+				return HandType.REPOKER
+			case [(_, 3), (_, 2), *_]:
+				return HandType.FULL_HOUSE
+			case [(_, 3), (_, 1), *_] if "J" in self.cards:
+				return HandType.POKER
+			case [(_, 3), (_, 1), *_]:
+				return HandType.TRIO
+			case [(_, 2), (_, 2), *_] if self.cards.count("J") == 2:
+				return HandType.POKER
+			case [(_, 2), (_, 2), *_] if "J" in self.cards:
+				return HandType.FULL_HOUSE
+			case [(_, 2), (_, 2), *_]:
+				return HandType.TWO_PAIR
+			case [(_, 2), (_, 1), *_] if "J" in self.cards:
+				return HandType.TRIO
+			case [(_, 2), (_, 1), *_]:
+				return HandType.PAIR
+			case _ if "J" in self.cards:
+				return HandType.PAIR
+			case _:
+				return HandType.HIGH_CARD
 
 	def __eq__(self, other: Self):
 		return other.cards == self.cards
 
 	def __lt__(self, other: Self):
 		if self.type != other.type:
-			return self.type < other.type
+			return self.type.value < other.type.value
 		for sc, oc in zip(self.cards, other.cards):
 			if sc != oc:
 				return card2value[sc] < card2value[oc]
